07f29fb133d9dc59da6f6e745863071e13fd846c
shibatakousei <shibata.k@inon.tech>
2022-12-02 02:40:15 +0900

    pc_footer-完成

--- wp-content/themes/MainTheme/footer.php
+++ wp-content/themes/MainTheme/footer.php
@@ -1,5 +1,15 @@
 <footer class="l-footer">
+  <div class="l-footer__wrap">
+    <div class="l-inner">
+      <h1>TMSON</h1>
+      <p class="l-footer__access">〒399-4511　長野県上伊那郡南箕輪村田畑 6160-14<br>
+        FAX 0265-73-7080
+      </p>
 
+      <a href="tel:02657375555">0265-73-7555</a>
+      <p class="l-footer__time">平日 8:00〜17:00</p>
+    </div>
+  </div>
 </footer>
 
 <?php wp_footer(); ?>
