9f9e80164ae342c053f465d0009d4bddc92615e1
shibatakousei <shibata.k@inon.tech>
2022-12-02 02:05:49 +0900

    pc_recruitセクション-3カラムの写真を中央に修正、pc_mapセクション完

--- wp-content/themes/MainTheme/front-page.php
+++ wp-content/themes/MainTheme/front-page.php
@@ -140,5 +140,11 @@ Template Name:front-page
       </div>
     </div>
   </section>
+
+  <section class="p-frontMap">
+    <div class="gmap">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3233.336517116516!2d137.98001831544994!3d35.865269327463764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601cf0dd49639945%3A0x4feeff05fad703ce!2z44OG44Og44K944Oz!5e0!3m2!1sja!2sjp!4v1669913889710!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <div>
+  </section>
 </main>
 <?php get_footer(); ?>
