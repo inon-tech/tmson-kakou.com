f5c88dff2a8fe44770484197383dfe23a264d093
shibatakousei <shibata.k@inon.tech>
2022-12-05 06:32:33 +0900

    headerのリンク設定

--- wp-content/themes/MainTheme/header.php
+++ wp-content/themes/MainTheme/header.php
@@ -25,7 +25,7 @@
 <body>
   <header class="l-header">
     <div class="l-headerPC">
-      <div class="l-headerPC__logo"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/public/img/normal/1x/common/header-logo.png" alt=""></div>
+      <div class="l-headerPC__logo"><a href="<?php echo esc_url(home_url('')); ?>"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/public/img/normal/1x/common/header-logo.png" alt=""></a></div>
       <nav class="l-headerPC__nav">
         <ul>
           <li>
@@ -34,7 +34,7 @@
             </a>
           </li>
           <li>
-            <a href="<?php echo esc_url(home_url('one-stop')); ?>" class="bold">
+            <a href="<?php echo esc_url(home_url('strength')); ?>" class="bold">
               テムソンの強み
             </a>
           </li>
@@ -73,7 +73,9 @@
       </nav>
     </div>
     <div class="l-headerSP">
-      <div class="l-headerSP__logo"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/public/img/normal/1x/common/header-logo-sp.png" alt=""></div>
+      <div class="l-headerSP__logo">
+        <a href="<?php echo esc_url(home_url('')); ?>"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/public/img/normal/1x/common/header-logo-sp.png" alt=""></a>
+      </div>
 
       <div class="l-headerSP__wrap js-headerSP__wrap">
         <div class="l-headerSP__ME bold">MENU</div>
@@ -88,13 +90,13 @@
 
       <div class="l-headerSP__content js-headerSP__content">
         <ul>
-          <li><a class="bold" href="">HOME</a></li>
-          <li><a class="bold" href="">会社概要</a></li>
-          <li><a class="bold" href="">テムソンの強み</a></li>
-          <li><a class="bold" href="">主要設備紹介</a></li>
-          <li><a class="bold" href="">お知らせ</a></li>
-          <li><a class="bold" href="">採用情報</a></li>
-          <li><a class="bold" href="">お問い合わせ</a></li>
+          <li><a class="bold" href="<?php echo esc_url(home_url('')); ?>">HOME</a></li>
+          <li><a class="bold" href="<?php echo esc_url(home_url('company')); ?>">会社概要</a></li>
+          <li><a class="bold" href="<?php echo esc_url(home_url('strength')); ?>">テムソンの強み</a></li>
+          <li><a class="bold" href="<?php echo esc_url(home_url('facility')); ?>">主要設備紹介</a></li>
+          <li><a class="bold" href="<?php echo esc_url(home_url('news')); ?>">お知らせ</a></li>
+          <li><a class="bold" href="<?php echo esc_url(home_url('recruit')); ?>">採用情報</a></li>
+          <li><a class="bold" href="<?php echo esc_url(home_url('contact')); ?>">お問い合わせ</a></li>
         </ul>
 
         <div class="l-headerSP__contentBt">
