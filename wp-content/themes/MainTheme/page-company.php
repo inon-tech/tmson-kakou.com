c1c8c2308470cee608c63f4d41d7410a72e8be49
shibatakousei <shibata.k@inon.tech>
2022-12-05 04:19:36 +0900

    PC_company_factoryセクション-完成

--- wp-content/themes/MainTheme/page-company.php
+++ wp-content/themes/MainTheme/page-company.php
@@ -74,85 +74,95 @@ Template Name:会社概要
   </section>
 
   <section class="p-companyHistory">
-      <h2 class="c-section-title">テムソンの歩み</h2>
-      <div class="p-companyHistory__list">
-        <div class="p-companyHistory__item">
-          <div class="p-companyHistory__left">
-            <div class="p-companyHistory__leftImg">
-            </div>
-          </div>
-          <div class="p-companyHistory__right">
-            <div class="p-companyHistory__rightTop">
-              <h3><span>episode 01</span>社屋と工場だけ</h3>
-            </div>
-            <div class="p-companyHistory__rightBottom">
-              <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
-            </div>
+    <h2 class="c-section-title">テムソンの歩み</h2>
+    <div class="p-companyHistory__list">
+      <div class="p-companyHistory__item">
+        <div class="p-companyHistory__left">
+          <div class="p-companyHistory__leftImg">
           </div>
         </div>
-        <div class="p-companyHistory__item">
-          <div class="p-companyHistory__left">
-            <div class="p-companyHistory__leftImg">
-            </div>
+        <div class="p-companyHistory__right">
+          <div class="p-companyHistory__rightTop">
+            <h3><span>episode 01</span>社屋と工場だけ</h3>
           </div>
-          <div class="p-companyHistory__right">
-            <div class="p-companyHistory__rightTop">
-              <h3><span>episode 02</span>異業種から製造業へ</h3>
-            </div>
-            <div class="p-companyHistory__rightBottom">
-              <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
-            </div>
-          </div>
-        </div>
-        <div class="p-companyHistory__item">
-          <div class="p-companyHistory__left">
-            <div class="p-companyHistory__leftImg">
-            </div>
-          </div>
-          <div class="p-companyHistory__right">
-            <div class="p-companyHistory__rightTop">
-              <h3><span>episode 03</span>５つの案件 - A社とのお取り引き</h3>
-            </div>
-            <div class="p-companyHistory__rightBottom">
-              <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
-            </div>
-          </div>
-        </div>
-        <div class="p-companyHistory__item">
-          <div class="p-companyHistory__left">
-            <div class="p-companyHistory__leftImg">
-            </div>
-          </div>
-          <div class="p-companyHistory__right">
-            <div class="p-companyHistory__rightTop">
-              <h3><span>episode 04</span>修練の日々</h3>
-            </div>
-            <div class="p-companyHistory__rightBottom">
-              <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
-            </div>
-          </div>
-        </div>
-        <div class="p-companyHistory__item">
-          <div class="p-companyHistory__left">
-            <div class="p-companyHistory__leftImg">
-            </div>
-          </div>
-          <div class="p-companyHistory__right">
-            <div class="p-companyHistory__rightTop">
-              <h3><span>episode 05</span>ご担当社様からのご退職のお言葉</h3>
-            </div>
-            <div class="p-companyHistory__rightBottom">
-              <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
-            </div>
+          <div class="p-companyHistory__rightBottom">
+            <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
           </div>
         </div>
       </div>
+      <div class="p-companyHistory__item">
+        <div class="p-companyHistory__left">
+          <div class="p-companyHistory__leftImg">
+          </div>
+        </div>
+        <div class="p-companyHistory__right">
+          <div class="p-companyHistory__rightTop">
+            <h3><span>episode 02</span>異業種から製造業へ</h3>
+          </div>
+          <div class="p-companyHistory__rightBottom">
+            <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-companyHistory__item">
+        <div class="p-companyHistory__left">
+          <div class="p-companyHistory__leftImg">
+          </div>
+        </div>
+        <div class="p-companyHistory__right">
+          <div class="p-companyHistory__rightTop">
+            <h3><span>episode 03</span>５つの案件 - A社とのお取り引き</h3>
+          </div>
+          <div class="p-companyHistory__rightBottom">
+            <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-companyHistory__item">
+        <div class="p-companyHistory__left">
+          <div class="p-companyHistory__leftImg">
+          </div>
+        </div>
+        <div class="p-companyHistory__right">
+          <div class="p-companyHistory__rightTop">
+            <h3><span>episode 04</span>修練の日々</h3>
+          </div>
+          <div class="p-companyHistory__rightBottom">
+            <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-companyHistory__item">
+        <div class="p-companyHistory__left">
+          <div class="p-companyHistory__leftImg">
+          </div>
+        </div>
+        <div class="p-companyHistory__right">
+          <div class="p-companyHistory__rightTop">
+            <h3><span>episode 05</span>ご担当社様からのご退職のお言葉</h3>
+          </div>
+          <div class="p-companyHistory__rightBottom">
+            <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 
   <section class="p-companyFactory">
-    <div class="l-inner">
-      <div class="p-companyFactory__top"></div>
-      <div class="p-companyFactory__bottom"></div>
+    <div class="p-companyFactory__list">
+      <div class="p-companyFactory__item">
+        <div class="p-companyFactory__left"></div>
+        <div class="p-companyFactory__right">
+          <p>第１工場</p>
+        </div>
+      </div>
+      <div class="p-companyFactory__item">
+        <div class="p-companyFactory__left">
+          <p>第２工場</p>
+        </div>
+        <div class="p-companyFactory__right"></div>
+      </div>
     </div>
   </section>
 
