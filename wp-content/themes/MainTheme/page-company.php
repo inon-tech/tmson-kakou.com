77a21ec7a7fa65b6901b1617dd76547805e9ccec
shibatakousei <shibata.k@inon.tech>
2022-12-04 23:48:32 +0900

    PC_companyページ-必要な画像とファイルの追加、html完成

--- wp-content/themes/MainTheme/page-company.php
+++ wp-content/themes/MainTheme/page-company.php
@@ -8,7 +8,136 @@ global $IHY_img_2; ?>
 Template Name:会社概要
 */
 ?>
-<main class="">
+<main class="p-company">
+  <section class="p-companyFV l-page-FV">
+    <h1 class="l-page-FV__title"><img src="<?php echo esc_url(get_template_directory_uri()) . $IHY_img_1 ?>company/company-title.png" alt=""></h1>
+    <div class="l-page-FV__desc">
+      この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。
+      文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、
+      行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、
+      行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、
+      行間等を確認するために入れています。
+    </div>
+  </section>
+
+  <section class="p-companyOverview">
+    <h2 class="c-section-title">会社概要</h2>
+    <div class="l-inner">
+      <div class="p-companyOverview__list">
+        <div class="p-companyOverview__item">
+          <div class="p-companyOverview__left">社名</div>
+          <div class="p-companyOverview__right">株式会社テムソン</div>
+        </div>
+        <div class="p-companyOverview__item">
+          <div class="p-companyOverview__left">創業</div>
+          <div class="p-companyOverview__right">平成4年5月1日</div>
+        </div>
+        <div class="p-companyOverview__item">
+          <div class="p-companyOverview__left">資本金</div>
+          <div class="p-companyOverview__right">1,000万円</div>
+        </div>
+        <div class="p-companyOverview__item">
+          <div class="p-companyOverview__left">代表者</div>
+          <div class="p-companyOverview__right">代表取締役 宮澤隆志</div>
+        </div>
+        <div class="p-companyOverview__item">
+          <div class="p-companyOverview__left">電話</div>
+          <div class="p-companyOverview__right">0265-73-7555</div>
+        </div>
+        <div class="p-companyOverview__item">
+          <div class="p-companyOverview__left">FAX</div>
+          <div class="p-companyOverview__right">0265-73-7080</div>
+        </div>
+        <div class="p-companyOverview__item">
+          <div class="p-companyOverview__left">本社・工場</div>
+          <div class="p-companyOverview__right">〒399-4511　長野県上伊那郡南箕輪村田端宇沖河原6160-14</div>
+        </div>
+        <div class="p-companyOverview__item">
+          <div class="p-companyOverview__left">第２工場</div>
+          <div class="p-companyOverview__right">〒399-4511　長野県上伊那郡南箕輪村田端宇沖河原6160-14</div>
+        </div>
+        <div class="p-companyOverview__item">
+          <div class="p-companyOverview__left">事業内容</div>
+          <div class="p-companyOverview__right">
+            <p>精密機器・精密部品製造</p>
+            <p>機械工具設計・製造</p>
+            <p>工作機器設計・製造</p>
+            <p>自社開発製品「楽まきくん」製造・販売</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="p-companyHistory">
+    <div class="l-inner">
+      <h2 class="c-section-title">テムソンの歩み</h2>
+      <div class="p-companyHistory__list">
+        <div class="p-companyHistory__item">
+          <div class="p-companyHistory__left"></div>
+          <div class="p-companyHistory__right">
+            <div class="p-companyHistory__rightTop">
+              <h3>社屋と工場だけ</h3>
+            </div>
+            <div class="p-companyHistory__rightBottom">
+              <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-companyHistory__item">
+          <div class="p-companyHistory__left"></div>
+          <div class="p-companyHistory__right">
+            <div class="p-companyHistory__rightTop">
+              <h3>異業種から製造業へ</h3>
+            </div>
+            <div class="p-companyHistory__rightBottom">
+              <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-companyHistory__item">
+          <div class="p-companyHistory__left"></div>
+          <div class="p-companyHistory__right">
+            <div class="p-companyHistory__rightTop">
+              <h3>５つの案件 - A社とのお取り引き</h3>
+            </div>
+            <div class="p-companyHistory__rightBottom">
+              <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-companyHistory__item">
+          <div class="p-companyHistory__left"></div>
+          <div class="p-companyHistory__right">
+            <div class="p-companyHistory__rightTop">
+              <h3>修練の日々</h3>
+            </div>
+            <div class="p-companyHistory__rightBottom">
+              <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
+            </div>
+          </div>
+        </div>
+        <div class="p-companyHistory__item">
+          <div class="p-companyHistory__left"></div>
+          <div class="p-companyHistory__right">
+            <div class="p-companyHistory__rightTop">
+              <h3>ご担当社様からのご退職のお言葉</h3>
+            </div>
+            <div class="p-companyHistory__rightBottom">
+              <p>この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れ</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="p-companyFactory">
+    <div class="l-inner">
+      <div class="p-companyFactory__top"></div>
+      <div class="p-companyFactory__bottom"></div>
+    </div>
+  </section>
 
 </main>
 <?php get_footer(); ?>
