57a497125a0305d970e9d1774ab6a6839a1422cc
pedro-inon-tech <tadano.ph@inon.tech>
2022-12-14 22:41:30 +0900

    npx gulp build

--- wp-content/themes/MainTheme/dev/gulpfile.js
+++ wp-content/themes/MainTheme/dev/gulpfile.js
@@ -35,11 +35,12 @@ function watch() {
 	gulp.watch("./src/assets/sass/**/*.scss", gulp.series(compileSass, browserReload));
 	gulp.watch("./src/assets/js/**/*.js", gulp.series(minJS, browserReload));
 	gulp.watch("./src/assets/img/**/*", gulp.series(copyImage, browserReload));
+	gulp.watch("../*.php", gulp.series(browserReload));
 }
 
 function browserInit(done) {
 	browserSync.init({
-		proxy: "http://005tmsonkakou.local/"
+		proxy: "http://tmson-kakou.com.local/"
 	});
 	done();
 }
