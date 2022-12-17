a3868e2db9c8cfdf59eef446438f7248e98809d9
pedro-inon-tech <tadano.ph@inon.tech>
2022-12-17 14:41:18 +0900

    page-company.phpだけ、パンくずが太字になっていたため修正

--- wp-content/themes/MainTheme/page-company.php
+++ wp-content/themes/MainTheme/page-company.php
@@ -21,7 +21,7 @@ Template Name:会社概要
         </h1>
       </div>
       <div class="l-page-FV__desc">
-        <div class="l-breadcrumb bold">
+        <div class="l-breadcrumb">
           <?php bcn_display(); ?>
         </div>
         この文章はダミーです。文字の大きさ、量、字間、行間等を確認するために入れています。この文章はダミーです。
